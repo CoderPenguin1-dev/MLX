092905a87f35f27112017e409bf1a0b1cafe7940
CoderPenguin1-dev <coderpenguin1.dev@gmail.com>
2025-10-27 17:16:52 -0400

    Add GPLv3 notice to all files

--- MLX/Program.cs
+++ MLX/Program.cs
@@ -1,4 +1,22 @@
-﻿using Avalonia;
+﻿/*
+   MLX - A cross-platform Doom/id Tech 1 launcher
+   Copyright (C) 2025 CoderPenguin1 @ coderpenguin1.dev@gmail.com
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using Avalonia;
 using System;
 
 namespace MLX;
